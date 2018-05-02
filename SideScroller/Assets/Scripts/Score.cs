84a3cb6d827a1095888da226ce2c305ccd0f8f4a
leonwan23 <leon_wan23@hotmail.com>
2018-05-02 20:30:50 +0800

    added coin, edited enemy prefab, removed cloud sprite

--- SideScroller/Assets/Scripts/Score.cs
+++ SideScroller/Assets/Scripts/Score.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Score : MonoBehaviour {
@@ -17,4 +18,13 @@ public class Score : MonoBehaviour {
 	void Update () {
         scoreDisplay.text = "Score: " + playerScore;
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "coin")
+        {
+            playerScore += 10;
+            Destroy(collision.gameObject);
+        }
+    }
 }
